1e29851c787fbde6fd27fb1dc009a36da92c89c6
sabrinaliu <sliu.owl68@yahoo.com>
2020-08-19 03:09:14 -0400

    Updates

---
+++ precache-manifest.bdb0a9a74f7c5f982bc83941db11bcb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b269426312cf23dd6529fb8f016fc5",
+    "url": "/EE-biomedical-racial-bias/index.html"
+  },
+  {
+    "revision": "07cf36bab0bc099f4ca8",
+    "url": "/EE-biomedical-racial-bias/static/css/main.4454ef51.chunk.css"
+  },
+  {
+    "revision": "37c77b21768946aa3238",
+    "url": "/EE-biomedical-racial-bias/static/js/2.1590ed17.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/EE-biomedical-racial-bias/static/js/2.1590ed17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07cf36bab0bc099f4ca8",
+    "url": "/EE-biomedical-racial-bias/static/js/main.9d05bbb2.chunk.js"
+  },
+  {
+    "revision": "074041642419d051991c",
+    "url": "/EE-biomedical-racial-bias/static/js/runtime-main.eb1ea0fa.js"
+  },
+  {
+    "revision": "72777e22cdec5d7c04f7d31d49cb60ad",
+    "url": "/EE-biomedical-racial-bias/static/media/lungs_background.72777e22.jpg"
+  },
+  {
+    "revision": "368ed67be6e20e612ca0913249fc7b74",
+    "url": "/EE-biomedical-racial-bias/static/media/spiro_test.368ed67b.webp"
+  }
+]);
